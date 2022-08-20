8b88698c5f2319c6e3554ac7d9e0e1391acdd35d
Rishit Dagli <rishit.dagli@gmail.com>
2022-08-20 17:21:05 +0000

    Format with black

--- nystromformer/test_nystromformer.py
+++ nystromformer/test_nystromformer.py
@@ -8,6 +8,7 @@ from parameterized import parameterized
 
 from .nystromformer import Nystromformer
 
+
 class NystromAttentionTest(tf.test.TestCase):
     def setUp(self):
         super(NystromAttentionTest, self).setUp()
@@ -18,25 +19,19 @@ class NystromAttentionTest(tf.test.TestCase):
         for _ in range(8):
             dim.append(random.randint(1, 1000))
 
-        param_list = [
-            [a]
-            for a in dim
-        ]
+        param_list = [[a] for a in dim]
         return param_list
 
     @parameterized.expand(generate_params())
     def test_shape_and_rank(self, dim):
-        model = Nystromformer(dim=dim,
-                            dim_head = 64,
-                                heads = 8,
-                                depth = 6,
-                                num_landmarks = 256,
-                                pinv_iterations = 6)
-        
+        model = Nystromformer(
+            dim=dim, dim_head=64, heads=8, depth=6, num_landmarks=256, pinv_iterations=6
+        )
+
         x = tf.random.uniform((1, 16384, dim))
         mask = tf.ones([1, 16384], dtype=tf.bool)
 
-        y = model(x, mask = mask)
+        y = model(x, mask=mask)
 
         self.assertEqual(tf.rank(y), 3)
         self.assertShapeEqual(np.zeros((1, 16384, dim)), y)
