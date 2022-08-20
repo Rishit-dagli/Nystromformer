bdd8742e37fa6e843ede45576652d506f92b71b4
Rishit Dagli <rishit.dagli@gmail.com>
2022-08-20 10:11:17 +0000

    Lint with black and isort

--- nystromformer/nystromformer.py
+++ nystromformer/nystromformer.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
-from .utils import PreNorm, FeedForward
+
 from .nystrom_attention import NystromAttention
+from .utils import FeedForward, PreNorm
+
 
 class Nystromformer(tf.keras.Model):
     def __init__(
@@ -8,14 +10,14 @@ class Nystromformer(tf.keras.Model):
         *,
         dim,
         depth,
-        dim_head = 64,
-        heads = 8,
-        num_landmarks = 256,
-        pinv_iterations = 6,
-        attn_values_residual = True,
-        attn_values_residual_conv_kernel = 33,
-        attn_dropout = 0.,
-        ff_dropout = 0.,
+        dim_head=64,
+        heads=8,
+        num_landmarks=256,
+        pinv_iterations=6,
+        attn_values_residual=True,
+        attn_values_residual_conv_kernel=33,
+        attn_dropout=0.0,
+        ff_dropout=0.0,
         **kwargs
     ):
         super(Nystromformer, self).__init__(**kwargs)
@@ -24,11 +26,24 @@ class Nystromformer(tf.keras.Model):
 
         self.net = []
         for _ in range(depth):
-            self.net.append(PreNorm(NystromAttention(dim = dim, dim_head = dim_head, heads = heads, num_landmarks = num_landmarks, pinv_iterations = pinv_iterations, residual = attn_values_residual, residual_conv_kernel = attn_values_residual_conv_kernel, dropout = attn_dropout)))
-            self.net.append(PreNorm(FeedForward(dim = dim, dropout = ff_dropout)))
+            self.net.append(
+                PreNorm(
+                    NystromAttention(
+                        dim=dim,
+                        dim_head=dim_head,
+                        heads=heads,
+                        num_landmarks=num_landmarks,
+                        pinv_iterations=pinv_iterations,
+                        residual=attn_values_residual,
+                        residual_conv_kernel=attn_values_residual_conv_kernel,
+                        dropout=attn_dropout,
+                    )
+                )
+            )
+            self.net.append(PreNorm(FeedForward(dim=dim, dropout=ff_dropout)))
 
     def call(self, inputs, mask=None):
         for i in range(self.depth):
-            inputs = self.net[2*i](inputs, mask=mask)+inputs
-            inputs = self.net[(2*i) + 1](inputs)+inputs
+            inputs = self.net[2 * i](inputs, mask=mask) + inputs
+            inputs = self.net[(2 * i) + 1](inputs) + inputs
         return inputs
--- nystromformer/utils.py
+++ nystromformer/utils.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from einops import rearrange, reduce
 
+
 class MoorePenrosePseudoinverse(tf.keras.layers.Layer):
     def __init__(self, iteration=6, **kwargs):
         super(MoorePenrosePseudoinverse, self).__init__(**kwargs)
@@ -11,16 +12,32 @@ class MoorePenrosePseudoinverse(tf.keras.layers.Layer):
         abs_inputs = tf.abs(inputs)
         cols = tf.math.reduce_sum(abs_inputs, axis=-1)
         rows = tf.math.reduce_sum(abs_inputs, axis=-2)
-        z = rearrange(inputs, '... i j -> ... j i') / (tf.math.reduce_max(cols) * tf.math.reduce_max(rows))
+        z = rearrange(inputs, "... i j -> ... j i") / (
+            tf.math.reduce_max(cols) * tf.math.reduce_max(rows)
+        )
 
         identity = tf.eye(z.shape[-1])
-        identity = rearrange(identity, 'i j -> () i j')
+        identity = rearrange(identity, "i j -> () i j")
 
         for _ in range(self.iteration):
             inputs_bbm_z = inputs @ z
-            z = 0.25 * z @ (13 * identity - (inputs_bbm_z @ (15 * identity - (inputs_bbm_z @ (7 * identity - inputs_bbm_z)))))
-        
+            z = (
+                0.25
+                * z
+                @ (
+                    13 * identity
+                    - (
+                        inputs_bbm_z
+                        @ (
+                            15 * identity
+                            - (inputs_bbm_z @ (7 * identity - inputs_bbm_z))
+                        )
+                    )
+                )
+            )
+
         return z
+
 
 class PreNorm(tf.keras.layers.Layer):
     def __init__(self, fn, **kwargs):
@@ -32,16 +49,19 @@ class PreNorm(tf.keras.layers.Layer):
         inputs = self.norm(inputs)
         return self.fn(inputs, **kwargs)
 
+
 class FeedForward(tf.keras.layers.Layer):
-    def __init__(self, dim, mult = 4, dropout = 0., **kwargs):
+    def __init__(self, dim, mult=4, dropout=0.0, **kwargs):
         super(FeedForward, self).__init__(**kwargs)
 
-        self.net = tf.keras.Sequential([
-            tf.keras.layers.Dense(dim * mult),
-            tf.keras.layers.Activation(tf.nn.gelu),
-            tf.keras.layers.Dropout(dropout),
-            tf.keras.layers.Dense(dim),
-        ])
+        self.net = tf.keras.Sequential(
+            [
+                tf.keras.layers.Dense(dim * mult),
+                tf.keras.layers.Activation(tf.nn.gelu),
+                tf.keras.layers.Dropout(dropout),
+                tf.keras.layers.Dense(dim),
+            ]
+        )
 
     def call(self, inputs):
         return self.net(inputs)
