a4f187579241e202bc046fe8eac58311a264e3f4
Rishit Dagli <rishit.dagli@gmail.com>
2022-08-20 07:56:06 +0000

    Create feed forward layer

--- nystromformer/utils.py
+++ nystromformer/utils.py
@@ -28,6 +28,20 @@ class PreNorm(tf.keras.layers.Layer):
         self.fn = fn
         self.norm = tf.keras.layers.LayerNormalization(axis=-1)
 
-    def call(self, x, **kwargs):
-        x = self.norm(x)
-        return self.fn(x, **kwargs)
+    def call(self, inputs):
+        inputs = self.norm(inputs)
+        return self.fn(inputs)
+
+class FeedForward(tf.keras.layers.Layer):
+    def __init__(self, dim, mult = 4, dropout = 0., **kwargs):
+        super(FeedForward, self).__init__(**kwargs)
+
+        self.net = tf.keras.Sequential([
+            tf.keras.layers.Dense(dim * mult),
+            tf.keras.layers.Activation(tf.nn.gelu),
+            tf.keras.layers.Dropout(dropout),
+            tf.keras.layers.Dense(dim),
+        ])
+
+    def call(self, inputs):
+        return self.net(inputs)
