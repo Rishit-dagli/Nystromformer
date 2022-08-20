19d0df53f52f8344ad5e3555ab913c183bbbc8fc
Rishit Dagli <rishit.dagli@gmail.com>
2022-08-20 09:51:45 +0000

    Nystromformer should be a Model not a Layer

--- nystromformer/nystromformer.py
+++ nystromformer/nystromformer.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from .utils import PreNorm, FeedForward
 from .nystrom_attention import NystromAttention
 
-class Nystromformer(tf.keras.layers.Layer):
+class Nystromformer(tf.keras.Model):
     def __init__(
         self,
         *,
@@ -22,13 +22,13 @@ class Nystromformer(tf.keras.layers.Layer):
 
         self.depth = depth
 
-        self.layers = []
+        self.net = []
         for _ in range(depth):
-            self.layers.append(PreNorm(NystromAttention(dim = dim, dim_head = dim_head, heads = heads, num_landmarks = num_landmarks, pinv_iterations = pinv_iterations, residual = attn_values_residual, residual_conv_kernel = attn_values_residual_conv_kernel, dropout = attn_dropout)))
-            self.layers.append(PreNorm(FeedForward(dim = dim, dropout = ff_dropout)))
+            self.net.append(PreNorm(NystromAttention(dim = dim, dim_head = dim_head, heads = heads, num_landmarks = num_landmarks, pinv_iterations = pinv_iterations, residual = attn_values_residual, residual_conv_kernel = attn_values_residual_conv_kernel, dropout = attn_dropout)))
+            self.net.append(PreNorm(FeedForward(dim = dim, dropout = ff_dropout)))
 
     def call(self, inputs, mask=None):
         for i in range(self.depth):
-            inputs = self.layers[2*i](inputs, mask=mask)+inputs
-            inputs = self.layers[(2*i) + 1](inputs)+inputs
+            inputs = self.net[2*i](inputs, mask=mask)+inputs
+            inputs = self.net[(2*i) + 1](inputs)+inputs
         return inputs
