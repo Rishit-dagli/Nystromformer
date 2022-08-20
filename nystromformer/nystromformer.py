bde1b723750931b7592feb4d1dec55fce6f2f851
Rishit Dagli <rishit.dagli@gmail.com>
2022-08-20 09:47:05 +0000

    Implement Nystromformer class

---
+++ nystromformer/nystromformer.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from .utils import PreNorm, FeedForward
+from .nystrom_attention import NystromAttention
+
+class Nystromformer(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        *,
+        dim,
+        depth,
+        dim_head = 64,
+        heads = 8,
+        num_landmarks = 256,
+        pinv_iterations = 6,
+        attn_values_residual = True,
+        attn_values_residual_conv_kernel = 33,
+        attn_dropout = 0.,
+        ff_dropout = 0.,
+        **kwargs
+    ):
+        super(Nystromformer, self).__init__(**kwargs)
+
+        self.depth = depth
+
+        self.layers = []
+        for _ in range(depth):
+            self.layers.append(PreNorm(NystromAttention(dim = dim, dim_head = dim_head, heads = heads, num_landmarks = num_landmarks, pinv_iterations = pinv_iterations, residual = attn_values_residual, residual_conv_kernel = attn_values_residual_conv_kernel, dropout = attn_dropout)))
+            self.layers.append(PreNorm(FeedForward(dim = dim, dropout = ff_dropout)))
+
+    def call(self, inputs, mask=None):
+        for i in range(self.depth):
+            inputs = self.layers[2*i](inputs, mask=mask)+inputs
+            inputs = self.layers[(2*i) + 1](inputs)+inputs
+        return inputs
